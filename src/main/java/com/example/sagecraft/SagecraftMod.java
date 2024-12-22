82d366b3135909a1d36637d5bed9a23230cf00af
WuLong <nikke.n@icloud.com>
2024-12-22 21:25:16 +0200

    why me!

---
+++ src/main/java/com/example/sagecraft/SagecraftMod.java
@@ -0,0 +1,10 @@
+package com.example.sagecraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("sagecraft")
+public class SagecraftMod {
+    public SagecraftMod() {
+        // Initialization code here
+    }
+}
